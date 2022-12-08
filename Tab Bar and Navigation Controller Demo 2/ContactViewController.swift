6f1c4f70b10c0810c5d991e5c08210790232aab5
OctoPol7 <thechumpower@gmail.com>
2022-12-07 17:28:22 -0800

    Initial Commit

---
+++ Tab Bar and Navigation Controller Demo 2/ContactViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactViewController.swift
+//  Tab Bar and Navigation Controller Demo 2
+//
+//  Created by laptop on 2022-11-21.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Contacts";
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tab Bar and Navigation Controller Demo 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab Bar and Navigation Controller Demo 2
+//
+//  Created by laptop on 2022-11-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
